7976d12f78f60bf0461b9137e8aafc1acfa3f366
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-07-12 13:29:50 +0800

    button updated

--- part_1d/src/App.js
+++ part_1d/src/App.js
@@ -1,64 +1,27 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
+const Display = (props) => <div>{props.value}</div>;
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
 );
 
 const App = () => {
-  // const [clicks, setClick] = useState({
-  //   left: 0,
-  //   right: 0,
-  // });
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  // const handleLeftClick = () => {
-  //   const newClicks = {
-  //     ...clicks,
-  //     left: clicks.left + 1,
-  //   };
-  //   setClick(newClicks);
-  // };
-  // const handleRightClick = () => {
-  //   const newClicks = {
-  //     ...clicks,
-  //     right: clicks.right + 1,
-  //   };
-  //   setClick(newClicks);
-  // };
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
-    <div className="App">
-      {left}
-      {/* <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button> */}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
-
-      {/* {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right} */}
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
+
 export default App;
