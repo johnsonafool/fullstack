5376e6b33b3b816bb086f27bc2909076c4bd891a
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-07-12 09:15:49 +0800

    part 1d uploaded

--- part_1d/src/App.js
+++ part_1d/src/App.js
@@ -1,15 +1,49 @@
 import { useState } from "react";
 
 const App = () => {
+  // const [clicks, setClick] = useState({
+  //   left: 0,
+  //   right: 0,
+  // });
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left: clicks.left + 1,
+  //   };
+  //   setClick(newClicks);
+  // };
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     right: clicks.right + 1,
+  //   };
+  //   setClick(newClicks);
+  // };
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div className="App">
       {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {right}
+      <p>{allClicks.join(" ")}</p>
+
+      {/* {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right} */}
     </div>
   );
 };
